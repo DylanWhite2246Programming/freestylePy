25e28c99e0ae3dbe4a113f9926d831853bae5664
DylanWhite2246Programming <whited200550@gmail.com>
2024-02-29 10:34:49 -0500

    created constant file

---
+++ assets/constants.py
@@ -0,0 +1,14 @@
+# constants.py
+
+# Colors
+MENU_BG_COLOR = "#79242F"
+BUTTON_BG_COLOR = "#aa9767"
+BUTTON_FG_COLOR = "#323e48"
+
+# Window Resolution
+RESOLUTION = "800x480"
+
+# File Paths
+USERS_CSV_PATH = "assets/users.csv"
+RECIPIES_PATH = "assets/recipies/"
+SOURCES_CSV_PATH = "assets/sources.csv"
