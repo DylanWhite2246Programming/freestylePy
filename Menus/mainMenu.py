17bb8a1ad69dd1cfdc433890161f5d0805657e7b
DylanWhite2246Programming <whited200550@gmail.com>
2024-03-12 10:53:11 -0400

    added adjust balance button

--- Menus/mainMenu.py
+++ Menus/mainMenu.py
@@ -3,6 +3,8 @@ from assets import constants
 from Menus.drinkMenu import drinkMenu
 
 def mainMenu(name,balance,admin):
+    #first row
+
     window = tk.Tk()
     window.geometry(constants.RESOLUTION)
     window.configure(bg=constants.MENU_BG_COLOR)
@@ -13,22 +15,29 @@ def mainMenu(name,balance,admin):
     name_var = tk.StringVar(value=name)
     balance_var = tk.StringVar(value='$'+str(balance))
 
-    name_label = tk.Label(window, text='Name: ', bg=constants.MENU_BG_COLOR, fg=constants.BUTTON_BG_COLOR)
+    name_label = tk.Label(window, text='Name: ', bg=constants.MENU_BG_COLOR, fg=constants.BUTTON_BG_COLOR , width=10)
     name_label.grid(row=0, column=0, padx=5,sticky='e')
-    name_entry = tk.Entry(window, textvariable=name_var, state='readonly', readonlybackground=constants.MENU_BG_COLOR, fg=constants.BUTTON_BG_COLOR)
+    #name_label.location(x=0,y=0)
+    name_entry = tk.Entry(window, textvariable=name_var, state='readonly', readonlybackground=constants.MENU_BG_COLOR, fg=constants.BUTTON_BG_COLOR,width=10)
     name_entry.grid(row=0, column=1, padx=5,sticky='w')
 
-    balance_label = tk.Label(window, text='Balance: ',bg=constants.MENU_BG_COLOR,fg=constants.BUTTON_BG_COLOR)
+    balance_label = tk.Label(window, text='Balance: ',bg=constants.MENU_BG_COLOR,fg=constants.BUTTON_BG_COLOR, width=10)
     balance_label.grid(row=0,column=2, padx=5,sticky='e')
-    balance_entry = tk.Entry(window, textvariable=balance_var, state='readonly',readonlybackground=constants.MENU_BG_COLOR, fg=constants.BUTTON_BG_COLOR)
+    balance_entry = tk.Entry(window, textvariable=balance_var, state='readonly',readonlybackground=constants.MENU_BG_COLOR, fg=constants.BUTTON_BG_COLOR, width=10)
     balance_entry.grid(row=0, column=3, padx=5,sticky='w')
 
     if admin:
-        appendUser_button = tk.Button(window, text='Add New User', bg=constants.BUTTON_BG_COLOR,fg=constants.BUTTON_FG_COLOR, width=10)
-        appendUser_button.grid(row=0,column=4,padx=5)
+        appendUser_button = tk.Button(window, text='Add New User', bg=constants.BUTTON_BG_COLOR,fg=constants.BUTTON_FG_COLOR, width=12)
+        appendUser_button.grid(row=0,column=5,padx=5)
+
+        adjustBalance_Button = tk.Button(window, text='Adjust Bal', bg=constants.BUTTON_BG_COLOR, fg=constants.BUTTON_FG_COLOR, width= 12)
+        adjustBalance_Button.grid(row=0,column=6,padx=5)
+
 
     logout_button = tk.Button(window, text='Logout',command=logout_button_pressed,bg=constants.BUTTON_BG_COLOR,fg=constants.BUTTON_FG_COLOR, width=10)
-    logout_button.grid(row=0, column=5, padx=5)
+    logout_button.grid(row=0, column=4, padx=5)
+
+    #second row and beyond
 
     drinks = ["SOTB", "Vodka Cran", "Screw Driver", "Jungle Juice",
                 "Negroni", "Cosmo", "Margarita", "Cum and Roke"]
